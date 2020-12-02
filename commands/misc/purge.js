bfcae544885a38214989c87fd65134e9975c451b
Nalyd1369 <nalyd1369@gmail.com>
2020-12-02 00:29:56 -0600

    Fix purge command

--- commands/misc/purge.js
+++ commands/misc/purge.js
@@ -16,11 +16,10 @@ module.exports = {
         if (amount > 100) return message.reply("You can't delete more than 100 messages at once!"); // Checks if the `amount` integer is bigger than 100
         if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
         
-        if (amount = 100) {
-            pass
-        } else {
+        if (amount != 100) {
             amount += 1
         }
+        
         await message.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
             message.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
         )});
